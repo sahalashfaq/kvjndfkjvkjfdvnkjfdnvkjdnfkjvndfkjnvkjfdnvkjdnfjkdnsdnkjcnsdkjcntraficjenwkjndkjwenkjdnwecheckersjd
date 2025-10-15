74e3788c7285f0ac0980d3b47a0cfd711373ed97
sahalashfaq <145576065+sahalashfaq@users.noreply.github.com>
2025-10-15 15:56:37 +0500

    Update Traffic.py

--- Traffic.py
+++ Traffic.py
@@ -71,7 +71,6 @@ if uploaded_file:
 
         # Initialize Chrome driver with enhanced options
         chrome_options = webdriver.ChromeOptions()
-        chrome_options.add_argument('--headless=new')  # Use new headless mode
         chrome_options.add_argument('--no-sandbox')
         chrome_options.add_argument('--disable-dev-shm-usage')
         chrome_options.add_argument('--disable-gpu')
