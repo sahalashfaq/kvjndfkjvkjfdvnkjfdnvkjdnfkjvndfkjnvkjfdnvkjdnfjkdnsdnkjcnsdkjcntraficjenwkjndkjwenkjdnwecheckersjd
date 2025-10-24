d2f7758b3652ab4bb1ee9d8c082bced0bae924b4
sahalashfaq <145576065+sahalashfaq@users.noreply.github.com>
2025-10-24 17:29:38 +0500

    Add setup script for Chrome and Chromedriver installation
    Install Chrome and Chromedriver for Streamlit Cloud and set environment variables for SeleniumBase.

--- setup.sh
+++ setup.sh
@@ -1,1 +1,8 @@
+#!/bin/bash
+# Install Chrome & Driver for Streamlit Cloud
+sudo apt-get update
+sudo apt-get install -y chromium-browser chromium-chromedriver
 
+# Set environment variables for SeleniumBase
+export CHROME_BIN=/usr/bin/chromium-browser
+export CHROMEDRIVER_PATH=/usr/bin/chromedriver
